c679cbcece424346165d54837af13c51a57028b6
Irina.ilesco <ilesco@mail.ru>
2022-06-23 00:36:42 +0300

    docs: create new file.algorithmic task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function toReadable (number) {
-  
+    let num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    let tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    if (number < 20) return num[number];
+    else if (number < 100) return tens[Math.floor((number/10)-2)] + ((number%10) ? " "+num[Math.floor(number%10)] : "");
+    return num[Math.floor(number/100)] +" hundred" + ((number%100) == 0 ? "" : " " + toReadable(number%100));
 }
